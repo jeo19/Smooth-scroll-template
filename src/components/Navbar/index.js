7900bab46c4c715d544e3423a017087f3ff707e2
jeo19 <terrakkk@outlook.com>
2022-03-28 12:42:58 +0800

    feat:Add the Nav, NavbarContainer, NavLogo components related NavBar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Nav, NavbarContainer, NavLogo } from "./NavBarElemnts";
+
+export const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="">dolla</NavLogo>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
