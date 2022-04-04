68118f93b95ccd168546715df817de8ac63d8692
jeo19 <terrakkk@outlook.com>
2022-04-04 22:24:10 +0800

    feat:Add the HeroContent component

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,13 +1,40 @@
-import React from "react";
-import { HeroContainer, HeroBg, VideoBg } from "./HeroElments";
-import Video from "../../videos/stars.mp4";
+import React, { useState } from "react";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElments";
+import { Button } from "../ButtonElements";
+import Video from "../../videos/video.mp4";
 
 function HeroSection() {
+  const [hover, setHover] = useState(false);
+  const onHover = () => {
+    setHover(!hover);
+  };
   return (
     <HeroContainer id="home">
       <HeroBg>
         <VideoBg autoPlay loop muted src={Video} type="video/mp4"></VideoBg>
       </HeroBg>
+      <HeroContent>
+        <HeroH1>Virtual Banking Made Easy</HeroH1>
+        <HeroP>
+          Sign up for a new account today and receive $250 in credit towards
+          your next payment.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+            Get started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
   );
 }
