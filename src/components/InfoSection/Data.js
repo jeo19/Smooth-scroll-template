dfe5f2699f53e3a021f53c2be419dffb58d82992
jeo19 <terrakkk@outlook.com>
2022-04-08 02:39:44 +0800

    add:Add a value of the UI variable by homeObjOne

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,1 +1,17 @@
-
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "premium Bank",
+  headLine: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Get started",
+  imgStart: true,
+  img: require("../../images/svg-1.svg"),
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
