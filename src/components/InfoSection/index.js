d0f435fbe27c756c6c4fceb2f3d3623f7ea9878d
jeo19 <terrakkk@outlook.com>
2022-04-09 11:53:31 +0800

    fix:Add the props to the Button component

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -28,6 +28,9 @@ const InfoSection = ({ homeObjOne }) => {
     alt,
     darkText,
     id,
+    primary,
+    dark,
+    dark2,
   } = homeObjOne;
   return (
     <>
@@ -40,7 +43,19 @@ const InfoSection = ({ homeObjOne }) => {
                 <Heading lightText={lightText}>{headLine}</Heading>
                 <Subtitle darkText={darkText}>{description}</Subtitle>
                 <BtnWrap>
-                  <Button to="home">{buttonLabel}</Button>
+                  <Button
+                    to="home"
+                    smooth={true}
+                    duration={500}
+                    spy={true}
+                    exact="true"
+                    offset={-80}
+                    primary={primary ? 1 : 0}
+                    dark={dark ? 1 : 0}
+                    dark2={dark2 ? 1 : 0}
+                  >
+                    {buttonLabel}
+                  </Button>
                 </BtnWrap>
               </TextWrapper>
             </Column1>
