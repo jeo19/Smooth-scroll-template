772297499b23489122d7ae7e0a051b1ac27d7c8e
jeo19 <terrakkk@outlook.com>
2022-04-11 23:27:58 +0800

    add:Add the homeObjTwo object variable

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -15,3 +15,21 @@ export const homeObjOne = {
   primary: true,
   darkText: false,
 };
+
+export const homeObjTwo = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "premium Bank",
+  headLine: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: require("../../images/svg-1.svg").default,
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
