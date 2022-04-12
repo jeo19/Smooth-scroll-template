18699f303cbb37873c5c92c35a81975123037f7b
jeo19 <terrakkk@outlook.com>
2022-04-12 22:44:21 +0800

    add:Add the homeObjTwo and homeObjThree object variable

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import HeroSection from "../components/HeroSection";
 import InfoSection from "../components/InfoSection";
-import { homeObjOne } from "../components/InfoSection/Data";
+import {
+  homeObjOne,
+  homeObjTwo,
+  homeObjThree,
+} from "../components/InfoSection/Data";
 
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
@@ -17,6 +21,8 @@ function Home() {
       <Navbar toggle={toggle} />
       <HeroSection />
       <InfoSection homeObjOne={homeObjOne} />
+      <InfoSection homeObjTwo={homeObjTwo} />
+      <InfoSection homeObjThree={homeObjThree} />
     </>
   );
 }
