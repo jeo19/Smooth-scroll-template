f40eae5c3b710ce4893d6dacb1b8582ebaa9a1d8
jeo19 <terrakkk@outlook.com>
2022-04-21 10:40:49 +0800

    chore:Add the Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle></FooterLinkTitle>
+              <FooterLink to="/signin">How it works</FooterLink>
+              <FooterLink to="/signin">Testimonials</FooterLink>
+              <FooterLink to="/signin">Careers</FooterLink>
+              <FooterLink to="/signin">Investors</FooterLink>
+              <FooterLink to="/signin">Terms of Services</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+      </FooterWrap>
+    </FooterContainer>
+  );
+}
+
+export default Footer;
